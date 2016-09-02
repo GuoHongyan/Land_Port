abbc3f8fee71bbcf55fa4126450d82626c07efcd
ghy <1724286863@qq.com>
2016-09-02 18:25:48 +0800

    了解Android下为适用不同屏幕所采取的技术，编程实现横屏时显示一个界面，竖屏时显示不同界面。 1.理解不同语言、不同分辨率、横屏竖屏等限定符的使用 2.编程实现横屏、竖屏显示不同界面，有时间的话为不同语言或分辨率也进行设置。

---
+++ app/src/main/java/com/example/land_port/Another.java
@@ -0,0 +1,16 @@
+package com.example.land_port;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 果红艳。 on 2016/8/31 0031.
+ */
+public class Another extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
